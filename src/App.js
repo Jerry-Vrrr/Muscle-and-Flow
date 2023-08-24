8b26b89b3bc8ee3903adf623c3e66588a4a4cc6e
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2023-08-24 12:42:36 -0600

    Add elements to program description area

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.scss"; 
 import Nav from "./Components/Nav";
 import MainImage from "./Components/MainImage";
+import Programs from "./Components/Programs";
 
 function App() { 
 return ( 
  <div className="wrapper"> 
   <Nav />
   <MainImage />
+  <Programs />
  </div> ); 
 } 
 export default App;
---
+++ src/Components/Intensity.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Logo from '../Images/MF Logo.png'
+
+export default function Intensity() {
+  return (
+    <div className='program'>
+        <img className='logo' src={Logo} alt="MF stylized logo" />
+        <h1 className='banner'>INTENSITY</h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        </p>
+        <h2>This Program is For You if...</h2>
+        <ul>
+            <li>reason one is this</li>
+            <li>reason two also</li>
+            <li>and reason three</li>
+            <li>fourth reason as well</li>
+        </ul>
+        </div>
+  )
+}
